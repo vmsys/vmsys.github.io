8c0d5a28b511bf7e1d9ec44275ea0ba19f1f1a64
YOYOU <yoyou_email@163.com>
2014-12-19 19:33:25 +0800

    m

---
+++ js/scrolldiv.js
@@ -0,0 +1,25 @@
+var timer;
+$(function(){
+    $(window).scroll(function(){
+      clearInterval(timer);
+      var topScroll=getScroll();
+      var topDiv="300px";
+      var top=topScroll+parseInt(topDiv);
+      timer=setInterval(function(){
+        //$(".test").css("top", top+"px");
+        $(".scrolldiv").animate({"top":top},500);
+        },500)
+      })
+    })
+function getScroll(){
+  var bodyTop = 0;  
+  if (typeof window.pageYOffset != 'undefined') {  
+    bodyTop = window.pageYOffset;  
+  } else if (typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') {  
+    bodyTop = document.documentElement.scrollTop;  
+  }  
+  else if (typeof document.body != 'undefined') {  
+    bodyTop = document.body.scrollTop;  
+  }  
+  return bodyTop
+}
