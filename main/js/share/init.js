22f771273c2f34849c20c7885c1380f4830e9639
YOYOU <yoyou_email@163.com>
2014-12-25 02:58:39 +0800

    m

--- main/js/share/init.js
+++ main/js/share/init.js
@@ -1,9 +1,9 @@
 /*在线演示公共底部*/
 $(function() {
   var buf = [];
-  buf.push('<script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
-  buf.push('<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-0608155192548477" data-ad-slot="8305246055"></ins>');
-  buf.push('<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>');
+  //buf.push('<script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+  //buf.push('<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-0608155192548477" data-ad-slot="8305246055"></ins>');
+ // buf.push('<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>');
   /*百度统计*/
   buf.push('<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "//hm.baidu.com/hm.js?3088a94e3b69516f6df1098fc49847c9";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm, s);})();</script>');
   /*百度分享*/
