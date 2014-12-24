052cb48f96ca58daeb2341c65d46fd43e8c0d4e2
YOYOU <yoyou_email@163.com>
2014-12-24 15:57:40 +0800

    m

--- js/audio.js
+++ js/audio.js
@@ -16,7 +16,7 @@ if (audio != null && audio.canPlayType && audio.canPlayType("audio/mp3"))
 {
   audio.src = "audio/aryx.ogg";
   audio.loop = "loop";
-  audio.control = "control";
+  audio.controls = "controls";
   audio.play();
 }
 
