bc34eee8f754f3026f9248704cd2b899a625a147
YOYOU <yoyou_email@163.com>
2015-02-07 09:15:17 +0800

    msg

--- ad.js
+++ ad.js
@@ -23,7 +23,7 @@ function openx(o,x)
     var cx = parseInt(o.style.width);  
     if(cx < x)  
     {  
-        o.style.width = (cx + Math.ceil((x-cx)/100)) +"px";  
+        o.style.width = (cx + Math.ceil((x-cx)/5)) +"px";  
     }  
     else  
     {  
@@ -37,7 +37,7 @@ function openy(o,y)
     var cy = parseInt(o.style.height);  
     if(cy < y)  
     {  
-        o.style.height = (cy + Math.ceil((y-cy)/100)) +"px";  
+        o.style.height = (cy + Math.ceil((y-cy)/5)) +"px";  
     }  
     else  
     {  
